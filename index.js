ea8ba03848772d9f5e371ab90ce72eaa000f9434
Mannan Handa <117028364+MannanHanda@users.noreply.github.com>
2022-12-11 00:55:20 +0530

    Add drum-kit essentials

---
+++ index.js
@@ -0,0 +1,65 @@
+var numOfDrumBtn=document.querySelectorAll('.drum').length;
+
+
+// Detects buttonpress
+for (var i = 0; i < numOfDrumBtn; i++) {
+    document.querySelectorAll('button')[i].addEventListener("click",function(){
+        var buttonInnerHTML=this.innerHTML;
+        playSound(buttonInnerHTML);
+        addRemoveClass(buttonInnerHTML);
+        });
+}
+
+// Detects keyboardpress
+ document.addEventListener('keydown',function(event){
+    playSound(event.key);
+    addRemoveClass(event.key);
+ });
+
+ 
+// Function for playing sound
+function playSound(key){
+    switch (key) {
+        case 'w':
+            var crash=new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+        case 'a':
+            var snare=new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+        case 's':
+            var tom_1=new Audio('sounds/tom-1.mp3');
+            tom_1.play();
+            break;
+        case 'd':
+            var tom_2=new Audio('sounds/tom-2.mp3');
+            tom_2.play();
+            break;
+        case 'j':
+            var tom_3=new Audio('sounds/tom-3.mp3');
+            tom_3.play();
+            break;
+        case 'k':
+            var tom_4=new Audio('sounds/tom-4.mp3');
+            tom_4.play();
+            break;
+        case 'l':
+            var kick=new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+        
+    
+        default:
+            
+    }
+}
+
+function addRemoveClass(key){
+    var activeButton=document.querySelector('.'+key);
+    activeButton.classList.add('pressed');
+
+    setTimeout(function(){
+        activeButton.classList.remove('pressed');
+    },100);
+}
